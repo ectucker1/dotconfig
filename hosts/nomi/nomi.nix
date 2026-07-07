1fe7581e26003b02d518813a1a36ad028152c672
Ella Tucker <ectucker1@gmail.com>
2026-07-06 22:51:10 -0500

    Initial Nomi host configuration with Windows dual-boot.

--- hosts/nomi/nomi.nix
+++ hosts/nomi/nomi.nix
@@ -1,8 +1,22 @@
-{ ... }: {
+{ lib, ... }: {
   imports = [
     ./hardware-configuration.nix
     ../../common/common.nix
   ];
 
   networking.hostName = "nomi";
+
+  # Dual-boot with Windows (installed on separate drive).
+  # systemd-boot only scans its own ESP, so it can't see Windows on another drive.
+  # So we use GRUB with os-prober instead.
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    device = "nodev";
+    useOSProber = true;
+    # Disable fancy GRUB rendering to keep it snappy.
+    font = null;
+    splashImage = null;
+  };
 }
